a1f49f3a6716668c1194d1005625f80cd9bded1b
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-04-19 12:56:18 -0700

    Home page hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,4 @@
     import Hero from "../lib/components/hero.svelte";
 </script>
 <Hero />
-<div class="container">Let's see where this div shows up</div>
+<div class="container">Home page content</div>
