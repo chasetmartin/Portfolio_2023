a73d927f0a876b353b310f1c5bcde4ddef703491
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-04-26 11:52:50 -0700

    about data server loading

--- src/lib/components/homefooter.svelte
+++ src/lib/components/homefooter.svelte
@@ -1,5 +1,5 @@
 <footer>
-    <h6>Built with SvelteKit &#128293; / utilizing &#9889 SSR and delivering &#10024; UX</h6>
+    <h6>Built from scratch using SvelteKit &#128293; utilizing &#9889 fast SSR and delivering &#10024; UX</h6>
 </footer>
 
 <style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,9 +3,80 @@
     import { slide } from 'svelte/transition';
     import { fade } from "svelte/transition";
 
+    export let data;
+
 </script>
 <!-- <div in:slide={{duration:1100, delay:300}}> -->
 <div in:fade={{duration:800}}>
-    <Abouthero />
-    <div class="container">Here is some copy about me...</div>
-</div>
+    <Abouthero {data}/>
+    <div class="container">
+        <details open>
+            <!-- svelte-ignore a11y-no-redundant-roles -->
+            <summary role="button">
+                Mission
+            </summary>
+        {data.about.content}
+        </details>
+    </div>
+    <div class="container">
+        <div class="grid">
+            <details open>
+                <!-- svelte-ignore a11y-no-redundant-roles -->
+                <summary role="button" class="contrast">
+                    Skills
+                </summary>
+            <div class="container">
+                {#each Object.keys(data.skills) as category}
+                <article>
+                <header>{category}</header>
+                <div>
+                <ul>
+                    {#each data.skills[category] as skill}
+                        <li>{skill}</li>
+                    {/each}
+                </ul>
+                </div>
+                </article>
+                {/each}
+            </div>
+            </details>
+            <details open>
+                <!-- svelte-ignore a11y-no-redundant-roles -->
+                <summary role="button" class="contrast">
+                    Experience
+                </summary>
+            <div class="container">
+                {#each data.experience as exp}
+          <article>
+            <header>{exp.position} | {exp.company} <br/> {exp.location} {exp.date_range}</header>
+            <div>
+              <ul>
+                {#if exp.skills !== ""}
+                  <li>Programming skills: {exp.skills}</li>
+                {/if}
+                {#if exp.description !== ""}
+                  <li>{exp.description}</li>
+                {/if}
+              </ul>
+            </div>
+            {#if exp.position2}
+            <footer>
+                {exp.position2} | {exp.company2} <br/> {exp.location2} {exp.date_range2}
+            </footer>
+            {/if}
+          </article>
+        {/each}
+            </div>
+            </details>
+        </div>
+    </div>
+    </div>
+
+<style>
+
+
+[role=button]:is([aria-current], :hover, :active, :focus) {
+  transform: scale(1.01);
+}
+
+</style>
