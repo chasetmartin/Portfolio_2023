43dcaedcaea6289c46f7bade682e67a68eb4d2df
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-05-08 14:57:54 -0700

    sitemap creation

---
+++ src/routes/sitemap.xml/+server.js
@@ -0,0 +1,40 @@
+export async function GET() {
+    return new Response(
+        `
+        <?xml version="1.0" encoding="UTF-8" ?>
+        <urlset
+            xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+            xmlns:xhtml="https://www.w3.org/1999/xhtml"
+            xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+            xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+            xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+            xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+        >
+        <url>
+        <loc>https://chasethomasmartin.com/</loc>
+        <lastmod>2023-05-08T21:49:33+00:00</lastmod>
+        <priority>1.00</priority>
+      </url>
+      <url>
+        <loc>https://chasethomasmartin.com/about</loc>
+        <lastmod>2023-05-08T21:49:33+00:00</lastmod>
+        <priority>0.80</priority>
+      </url>
+      <url>
+        <loc>https://chasethomasmartin.com/projects</loc>
+        <lastmod>2023-05-08T21:49:33+00:00</lastmod>
+        <priority>0.80</priority>
+      </url>
+      <url>
+        <loc>https://chasethomasmartin.com/blog</loc>
+        <lastmod>2023-05-08T21:49:33+00:00</lastmod>
+        <priority>0.80</priority>
+      </url>
+        </urlset>`.trim(),
+        {
+            headers: {
+                'Content-Type': 'application/xml'
+            }
+        }
+    );
+}
