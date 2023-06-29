6b4ded5590cc9ee69fde56561962532f2f1e3e0d
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-06-29 11:57:45 -0700

    projects readme markdown formatting

--- src/lib/cards/project.svelte
+++ src/lib/cards/project.svelte
@@ -1,28 +1,55 @@
 <script>
     export let repo;
     const sortedTopics = repo.topics.sort().join(' | ');
+
+    import Markdown from '@magidoc/plugin-svelte-marked'
+
 </script>
 
     <article>
-        <header>
+        <!-- <header>
             <div><a role="button" class="outline contrast" href="{repo.url}" target="_blank">{repo.name}</a></div>
-        </header>
+        </header> -->
         <div>
-            {#if repo.description !== null }
-            <div>
-            {repo.description}
-            </div>
-            <hr> 
-            <div>
-                README:
-            </div>
-            <pre>{repo.readme}</pre>
+            {#if repo.description !== null && repo.name === "River_Dipper" || repo.name === "Dipper_Recreation" || repo.name === "Portfolio_2023" }
+            <header>
+                <div >
+                    <h3>{repo.name}</h3>
+                </div>
+            </header> 
+                    <details open>
+                        <!-- svelte-ignore a11y-no-redundant-roles -->
+                        <summary>
+                            <h6>{repo.description}</h6>
+                        </summary>
+                        <Markdown
+            source={repo.readme}
+                />
+                    </details>
+            {:else}
+            <header>
+                <div>
+                    <h3>{repo.name}</h3>
+                </div>
+            </header> 
+                    <details>
+                        <!-- svelte-ignore a11y-no-redundant-roles -->
+                        <summary >
+                            <h6>{repo.description}</h6>
+                            click to expand
+                        </summary>
+                        <Markdown
+            source={repo.readme}
+                />
+                    </details>
             {/if}
         </div>
         <footer>
             <div class="grid">
+                <div><a href="{repo.url}">{repo.url}</a></div>
                 <div>{sortedTopics}</div>
             </div>
         </footer>
     </article>
+
   
